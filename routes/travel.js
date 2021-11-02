ec57f1339bd796124ae815e8ded59b3005aa2c6b
Kevin Chu <kvnchu2@gmail.com>
2021-11-02 11:05:51 -0700

    initial commit

---
+++ routes/travel.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+/* GET users listing. */
+router.get('/travel', function(req, res, next) {
+  axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${coordinates[y].results[0].position.lat},${coordinates[y].results[0].position.lon}:${coordinates[y+1].results[0].position.lat},${coordinates[y+1].results[0].position.lon}/json?key=atFqCv6vs5HzL0u9qS9G5HXnhdYAA6kv&departAt=${icbcArr[y].startTime}&traffic=true`)
+});
+
+module.exports = router;
+
