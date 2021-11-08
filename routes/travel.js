2dce0a83015acbb7ececa3db29ca041695a94744
Kevin Chu <kvnchu2@gmail.com>
2021-11-07 16:00:17 -0800

    add axios get request to travel route

--- routes/travel.js
+++ routes/travel.js
@@ -4,9 +4,10 @@ const axios = require('axios');
 
 /* GET users listing. */
 router.get('/travel', function(req, response, next) {
-  axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${coordinates[y].results[0].position.lat},${coordinates[y].results[0].position.lon}:${coordinates[y+1].results[0].position.lat},${coordinates[y+1].results[0].position.lon}/json?key=atFqCv6vs5HzL0u9qS9G5HXnhdYAA6kv&departAt=${icbcArr[y].startTime}&traffic=true`)
+  
+  axios.get(`https://api.tomtom.com/routing/1/calculateRoute/49.22469,-123.03518:49.23566,-123.12379/json?key=atFqCv6vs5HzL0u9qS9G5HXnhdYAA6kv&departAt=2021-11-05T15:30:00.000Z&traffic=true`)
     .then((res) => {
-      response.json(res);
+      console.log(res.data);
     });
 });
 
