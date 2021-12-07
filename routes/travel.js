da5756aa298e8e6ea6be1622cbeb2835fca94e36
Kevin Chu <kvnchu2@gmail.com>
2021-12-07 12:38:50 -0800

    add route params for calculateRoute

--- routes/travel.js
+++ routes/travel.js
@@ -3,15 +3,17 @@ const router = express.Router();
 const axios = require('axios');
 
 /* GET users listing. */
-router.get('/travel', function(req, response, next) {
-  
-  axios.get(`https://api.tomtom.com/routing/1/calculateRoute/49.22469,-123.03518:49.23566,-123.12379/json?key=atFqCv6vs5HzL0u9qS9G5HXnhdYAA6kv&departAt=2021-11-05T15:30:00.000Z&traffic=true`)
+router.get('/calculateRoute/:fromLat/:fromLon/:toLat/:toLon', function(req, response, next) {
+  const fromLat = req.params['fromLat'];
+  const fromLon = req.params['fromLon'];
+  const toLat = req.params['toLat'];
+  const toLon = req.params['toLon'];
+
+  axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${fromLat},${fromLon}:${toLat},${toLon}/json?key=atFqCv6vs5HzL0u9qS9G5HXnhdYAA6kv&departAt=2021-11-05T15:30:00.000Z&traffic=true`)
     .then((res) => {
-      console.log(res.data);
+      console.log(res.data.routes[0].summary);
     });
 });
 
 module.exports = router;
-
-
 
