a01880348b511413c8ab5ec82f09a3d62e8db143
Kevin Chu <kvnchu2@gmail.com>
2021-12-07 13:10:52 -0800

    add departTime to calculateRoute params

--- routes/travel.js
+++ routes/travel.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const axios = require('axios');
 
 /* GET travel time and mileage for route */
-router.get('/calculateRoute/:fromLat/:fromLon/:toLat/:toLon', function(req, response, next) {
+router.get('/calculateRoute/:fromLat/:fromLon/:toLat/:toLon/:departTime', function(req, response, next) {
   const fromLat = req.params['fromLat'];
   const fromLon = req.params['fromLon'];
   const toLat = req.params['toLat'];
   const toLon = req.params['toLon'];
+  const departTime = req.params['departTime'];
 
-  axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${fromLat},${fromLon}:${toLat},${toLon}/json?key=atFqCv6vs5HzL0u9qS9G5HXnhdYAA6kv&departAt=2021-11-05T15:30:00.000Z&traffic=true`)
+  axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${fromLat},${fromLon}:${toLat},${toLon}/json?key=atFqCv6vs5HzL0u9qS9G5HXnhdYAA6kv&departAt=${departTime}&traffic=true`)
     .then((res) => {
       console.log(res.data.routes[0].summary);
     });
@@ -17,3 +18,4 @@ router.get('/calculateRoute/:fromLat/:fromLon/:toLat/:toLon', function(req, resp
 
 module.exports = router;
 
+//http://localhost:8080/travel/calculateRoute/49.22469/-123.03518/49.23566/-123.12379/2021-11-05T15:30:00.000Z
