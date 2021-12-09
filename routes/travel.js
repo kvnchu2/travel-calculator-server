ff6268ea17f6e88a036d02704a61689789337d15
Kevin Chu <kvnchu2@gmail.com>
2021-12-09 09:35:30 -0800

    remove console log from travel route

--- routes/travel.js
+++ routes/travel.js
@@ -13,12 +13,14 @@ router.get('/calculateRoute/:fromLat/:fromLon/:toLat/:toLon/:departTime', functi
   const toLon = req.params['toLon'];
   const departTime = req.params['departTime'];
 
-  axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${fromLat},${fromLon}:${toLat},${toLon}/json?key=${process.env.TOMTOM_KEY}&departAt=${departTime}&traffic=true`)
-    .then((res) => {
-      console.log(res.data.routes[0].summary);
-    });
+  return axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${fromLat},${fromLon}:${toLat},${toLon}/json?key=${process.env.TOMTOM_KEY}&departAt=${departTime}&traffic=true`)
+    // .then((res) => {
+    //   console.log(res.data.routes[0].summary);
+    // });
 });
 
 module.exports = router;
 
 //http://localhost:8080/travel/calculateRoute/49.22469/-123.03518/49.23566/-123.12379/2021-11-05T15:30:00.000Z
+
+//https://travel-calculator-server.herokuapp.com/travel/calculateRoute/49.22469/-123.03518/49.23566/-123.12379/2021-11-05T15:30:00.000Z
