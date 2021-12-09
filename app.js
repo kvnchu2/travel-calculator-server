e5083bc852ebfff9e263017f95ca23f2c51eb333
Kevin Chu <kvnchu2@gmail.com>
2021-12-09 09:58:13 -0800

    remove console log from app.js

--- app.js
+++ app.js
@@ -43,8 +43,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-console.log(process.env.TOMTOM_KEY);
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
