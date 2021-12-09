6d5dd0b104fb3ef5a18a56202f30b7609ddfa00e
Kevin Chu <kvnchu2@gmail.com>
2021-12-09 13:28:28 -0800

    add new api axios get request

--- app.js
+++ app.js
@@ -9,21 +9,9 @@ const PORT = 8080;
 const indexRouter = require('./routes/index');
 const travelRouter = require('./routes/travel');
 
-const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
 const app = express();
-app.use(allowCrossDomain);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
