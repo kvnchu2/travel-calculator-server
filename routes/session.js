eea4e67958aa740df3d156fb8aa876926e339135
Kevin Chu <kvnchu2@gmail.com>
2022-09-05 18:21:39 -0700

    add sessions completed route in sessions

--- routes/session.js
+++ routes/session.js
@@ -7,5 +7,16 @@ module.exports = function(router, database) {
       })
       .catch(e => res.send(e));
   });
+
+  router.post('/completed/:name/:sessionsCompleted', (req, res) => {
+    const name = req.params["name"];
+    const sessionsCompleted = req.params["sessionsCompleted"];
+    database.updateSessionsCompleted(name, sessionsCompleted)
+      .then((results) => {
+        res.send(results);
+      })
+      .catch(e => res.send(e));
+  });
+
   return router;
 };
