357301df7aeca997424f98708dc2c7cfe96632c9
Kevin Chu <kvnchu2@gmail.com>
2022-09-29 09:37:36 -0700

    add cron scheduling

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require("cors");
+const cron = require("node-cron");
 
 const database = require('./routes/database');
 const indexRouter = require('./routes/index');
@@ -53,6 +54,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+cron.schedule('* * * * *', function() {
+  console.log('Process running every minute');
 });
 
 app.listen(process.env.PORT || 3000, function() {
