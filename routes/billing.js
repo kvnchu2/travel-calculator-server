3708047a5ab06b2fb2960c95873786c2aeb43878
Kevin Chu <kvnchu2@gmail.com>
2022-10-10 10:37:02 -0700

    change billing route from params to body

--- routes/billing.js
+++ routes/billing.js
@@ -1,9 +1,9 @@
 module.exports = function(router, database) {
 
-  router.post('/add/:company/:link', (req, res) => {
+  router.post('/add/:company', (req, res) => {
 
     const company = req.params["company"];
-    const link = req.params["link"];
+    const link = req.body["link"];
     database.addBillingLink(company, link)
       .then((results) => {
         res.send(results);
